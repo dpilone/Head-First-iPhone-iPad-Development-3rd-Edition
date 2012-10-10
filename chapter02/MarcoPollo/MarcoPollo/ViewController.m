48b43dd5f561ec351e84cfe04fe5da4c0a495b23
Dan Pilone <dan@element84.com>
2012-10-09 22:26:29 -0400

    Add hashy tag to tweets.

--- chapter02/MarcoPollo/MarcoPollo/ViewController.m
+++ chapter02/MarcoPollo/MarcoPollo/ViewController.m
@@ -29,8 +29,10 @@
 
 - (IBAction)postItButtonPressed:(id)sender {
   NSLog(@"Post It button was pressed: %@", self.tweetTextView.text);
+  NSString *tweetText = [NSString stringWithFormat:@"%@ #MarcoPollo", self.tweetTextView.text];
+  
   SLComposeViewController *composer = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
-  [composer setInitialText:self.tweetTextView.text];
+  [composer setInitialText:tweetText];
   [self presentViewController:composer animated:YES completion:nil];
 }
 @end
