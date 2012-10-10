6230f9b1a8983af80d7110249317c88146ff1d1f
Dan Pilone <dan@element84.com>
2012-10-09 22:22:30 -0400

    Add twitter support for sending message.

--- chapter02/MarcoPollo/MarcoPollo/ViewController.m
+++ chapter02/MarcoPollo/MarcoPollo/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "Social/Social.h"
 
 @interface ViewController ()
 
@@ -28,5 +29,8 @@
 
 - (IBAction)postItButtonPressed:(id)sender {
   NSLog(@"Post It button was pressed: %@", self.tweetTextView.text);
+  SLComposeViewController *composer = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
+  [composer setInitialText:self.tweetTextView.text];
+  [self presentViewController:composer animated:YES completion:nil];
 }
 @end
