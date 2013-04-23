6e2135c8d9363e794347e88ff084fc1de5be29da
Paul Pilone <paul@pilone.org>
2013-04-23 01:37:56 -0400

    Chapter 6: Adds Gilligizer app.

---
+++ chapter06/Gilligizer/GilligizerTests/GilligizerTests.h
@@ -0,0 +1,13 @@
+//
+//  GilligizerTests.h
+//  GilligizerTests
+//
+//  Created by Paul Pilone on 4/22/13.
+//  Copyright (c) 2013 Element 84, LLC. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface GilligizerTests : SenTestCase
+
+@end
---
+++ chapter06/Gilligizer/GilligizerTests/GilligizerTests.m
@@ -0,0 +1,32 @@
+//
+//  GilligizerTests.m
+//  GilligizerTests
+//
+//  Created by Paul Pilone on 4/22/13.
+//  Copyright (c) 2013 Element 84, LLC. All rights reserved.
+//
+
+#import "GilligizerTests.h"
+
+@implementation GilligizerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in GilligizerTests");
+}
+
+@end
