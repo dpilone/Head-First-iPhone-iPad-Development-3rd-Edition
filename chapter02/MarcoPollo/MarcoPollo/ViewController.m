03533f76550aea0d0c7789547f26b28864749865
Paul Pilone <paul@element84.com>
2013-10-14 18:21:05 -0400

    Chapter 2: Updates start code to Xcode 5 and iOS 7.

--- chapter02/MarcoPollo/MarcoPollo/ViewController.m
+++ chapter02/MarcoPollo/MarcoPollo/ViewController.m
@@ -7,7 +7,6 @@
 //
 
 #import "ViewController.h"
-#import "Social/Social.h"
 
 @interface ViewController ()
 
@@ -27,12 +26,4 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)postItButtonPressed:(id)sender {
-  NSLog(@"Post It button was pressed: %@", self.tweetTextView.text);
-  NSString *tweetText = [NSString stringWithFormat:@"%@ #MarcoPollo", self.tweetTextView.text];
-  
-  SLComposeViewController *composer = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
-  [composer setInitialText:tweetText];
-  [self presentViewController:composer animated:YES completion:nil];
-}
 @end
